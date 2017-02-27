2386f42017aae699eaff28b24846eef6f16a263a
fedora <fedora@localhost.localdomain>
2017-02-27 22:49:50 +0800

    finish copying code of chapter03

---
+++ chapter03/v1.2/.scull.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/fedora/Books/GITHUB/NOTES_ldd3/chapter03/v1.2/scull.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/fedora/Books/GITHUB/NOTES_ldd3/chapter03/v1.2/scull.ko /home/fedora/Books/GITHUB/NOTES_ldd3/chapter03/v1.2/scull.o /home/fedora/Books/GITHUB/NOTES_ldd3/chapter03/v1.2/scull.mod.o ;  true
---
+++ chapter03/v1.2/.tmp_versions/scull.mod
@@ -0,0 +1,3 @@
+/home/fedora/Books/GITHUB/NOTES_ldd3/chapter03/v1.2/scull.ko
+/home/fedora/Books/GITHUB/NOTES_ldd3/chapter03/v1.2/scull.o
+
